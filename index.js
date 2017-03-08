200c8dca88bcd3f8a5078b42935c89d2b6dda0fd
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-03-08 23:12:49 +0200

    module: rebuild

--- index.js
+++ index.js
@@ -1,52 +1,80 @@
 'use strict';
 
+// # Otto
+// Helps deal with CAs
+
+// Wrap index round edges
+// http://stackoverflow.com/questions/1082917/mod-of-negative-number-is-melting-my-brain
+var mod = function mod(a, b) {
+  return a - b * Math.floor(a / b);
+};
+var sum = function sum(a, b) {
+  return a + b;
+};
+var glu = function glu(a, b, i) {
+  return a | b << i;
+};
+
 var Otto = function Otto() {
   var _ref = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {},
-      _ref$span = _ref.span,
-      span = _ref$span === undefined ? 1 : _ref$span,
-      _ref$size = _ref.size,
-      size = _ref$size === undefined ? 0 : _ref$size,
       _ref$rule = _ref.rule,
-      rule = _ref$rule === undefined ? 90 : _ref$rule;
+      rule = _ref$rule === undefined ? 90 : _ref$rule,
+      _ref$rows = _ref.rows,
+      rows = _ref$rows === undefined ? 1 : _ref$rows,
+      _ref$cols = _ref.cols,
+      cols = _ref$cols === undefined ? 1 : _ref$cols,
+      _ref$span = _ref.span,
+      span = _ref$span === undefined ? 1 : _ref$span;
 
-  // Cells
-  var _grid = new Uint8Array(size);
+  // Grid size
+  var area = rows * cols;
 
-  // Lookup table
-  var ruleset = rule.toString(2).split('').reverse();
+  // The neighborhood, store distance from each neighbor
+  var hood = rows > 1 ? [0, cols, -cols, 1, -1] : function (arr) {
+    for (var i = 0, max = 1 + 2 * span; i < max; i += 1) {
+      arr.push(span - i);
+    }
 
-  // Wrap index round edges
-  // http://stackoverflow.com/questions/1082917/mod-of-negative-number-is-melting-my-brain/1082938#1082938
-  var getIndex = function getIndex(a, b) {
-    return a - b * Math.floor(a / b);
-  };
+    return arr;
+  }([]);
+
+  // Decide type of sum
+  var getSum = rows > 1 ? sum : glu;
 
   // Calculate state
   var getState = function getState(v, i, arr) {
-    var state = [];
+    var state = hood.map(function (diff) {
+      var site = mod(diff + i, arr.length);
+      var flag = arr[site];
 
-    // The neighborhood
-    for (var j = -span; j <= span; j += 1) {
-      var x = getIndex(i + j, arr.length);
-      var n = arr[x] || 0;
+      // Just works :)
+      return diff === 0 && flag === 1 && rows > 1 ? hood.length : flag;
+    }).reduce(getSum);
 
-      state.push(n);
-    }
-
-    return ruleset[parseInt(state.join(''), 2)];
+    return rule & 1 << state ? 1 : 0;
   };
 
-  // Flip cell in the middle
-  _grid[Math.floor(size * 0.5)] = 1;
+  // Cells, zero filled
+  var memo = new Int8Array(area);
 
-  return {
-    next: function next() {
-      return _grid.set(_grid.map(getState));
-    },
-    grid: function grid() {
-      return _grid;
-    }
-  };
+  // Result
+  var grid = void 0;
+
+  return function (seed) {
+    // Flip cell in middle on init
+    memo[seed] = 1;
+
+    return function () {
+      // Update
+      grid = memo;
+
+      // Save for later
+      memo = grid.map(getState);
+
+      // The previous generation
+      return grid;
+    };
+  }(Math.floor(area * 0.5));
 };
 
 module.exports = Otto;
