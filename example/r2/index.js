da568cf73b94a98db5ea98223b57281b15751921
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-03-10 23:36:31 +0200

    example: fix per latest changes

--- example/r2/index.js
+++ example/r2/index.js
@@ -8,11 +8,7 @@ var plots = document.getElementsByTagName('canvas');
 var rules = [2123739367, 988197457, 2530535241, 404545713, 1931232828, 2713874006];
 var ottos = [];
 
-var cols = 179;
-var rows = 1;
-var span = 2;
-
-var framesN = cols;
+var framesN = 179;
 var frameId;
 
 var frame = function frame() {
@@ -22,7 +18,7 @@ var frame = function frame() {
     var grid = otto();
 
     for (var j = 0; j < grid.length; j += 1) {
-      var x = j % cols;
+      var x = j % framesN;
       var y = frameId - 1;
 
       if (grid[j]) {
@@ -48,10 +44,16 @@ if (window !== window.top) {
   html.className += ' is-iframe';
 }
 
-label.innerHTML = rules.join(', ');
+label.innerHTML = rules.join(' / ');
 
 rules.forEach((rule, i) => {
-  var otto = Otto({ rule, cols, rows, span });
+  var papa = {
+    rule: rule,
+    size: framesN,
+    ends: [-2, -1, 0, 1, 2],
+  };
+
+  var otto = Otto(papa);
 
   ottos.push(otto);
 });
