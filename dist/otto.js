68152b7537d834a7227d07bb03af4890e2715a53
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-06-25 21:59:48 +0200

    module: cleanup, rebuild

--- dist/otto.js
+++ dist/otto.js
@@ -1,31 +1,34 @@
 var Otto = (function () {
-  'use strict';
+'use strict';
 
-  // # Otto
-  // Helps deal CAs
+// # Otto
+// Helps deal CAs
 
-  // Wrap index round edges
-  // http://stackoverflow.com/questions/1082917/mod-of-negative-number-is-melting-my-brain
-  var myMod = function myMod(a, b) {
-    return a - b * Math.floor(a / b);
-  };
+// Wrap index round edges
+// http://stackoverflow.com/questions/1082917/mod-of-negative-number-is-melting-my-brain
+var myMod = function myMod(a, b) {
+  return a - b * Math.floor(a / b);
+};
+
+// Rule to binary convert
+var parseRule = function parseRule(rule) {
+  // Base 2 digits
+  var code = Number(rule).toString(2);
+
   var zeros = 1024 .toString(2).split('').slice(1).join('');
   var zerosMax = zeros.length;
 
-  // Rule to binary convert
-  var parseRule = function parseRule(rule) {
-    // Base 2 digits
-    var code = Number(rule).toString(2);
+  // No padding past 10
+  var diff = Math.max(zerosMax, zerosMax - code.length);
 
-    // No padding past 10
-    var diff = Math.max(zerosMax, zerosMax - code.length);
+  // Zero pad ruleset if need be
+  return ('' + zeros + code).substr(diff).split('').reverse();
+};
 
-    // Zero pad ruleset if need be
-    return ('' + zeros + code).substr(diff).split('').reverse();
-  };
-
-  // Defaults
-  var data = {
+// Maker
+var Otto = function Otto(options) {
+  // Merge options and defaults
+  var settings = Object.assign({
     size: 1,
     rule: 30,
 
@@ -44,52 +47,39 @@ var Otto = (function () {
 
       return code[stats];
     }
+  }, options);
+
+  // Rule 90 would be
+  // ```['0', '1', '0', '1', '1', '0', '1']```
+  var code = parseRule(settings.rule);
+
+  // Calculate state
+  var step = function step(v, i, view) {
+    // Collect neighboring flags
+    var hood = settings.ends.map(function (span) {
+      // The index for each neighbor
+      var site = myMod(span + i, view.length);
+
+      // The state of each neighbor
+      return view[site];
+    });
+
+    return settings.stat(hood, code, v);
   };
 
-  // Setup
-  var Otto = function Otto(opts) {
-    // Merge options and defaults
-    var _Object$assign = Object.assign({}, data, opts),
-        size = _Object$assign.size,
-        rule = _Object$assign.rule,
-        ends = _Object$assign.ends,
-        stat = _Object$assign.stat,
-        seed = _Object$assign.seed;
+  // Clipboard, zero filled
+  var grid = new Uint8Array(settings.size);
+  var next = settings.seed;
 
-    // Rule 90 would be
-    // ```['0', '1', '0', '1', '1', '0', '1']```
+  // Tick
+  return function () {
+    grid = grid.map(next);
+    next = step;
 
-
-    var code = parseRule(rule);
-
-    // Calculate state
-    var step = function step(v, i, view) {
-      // Collect neighboring flags
-      var hood = ends.map(function (span) {
-        // The index for each neighbor
-        var site = myMod(span + i, view.length);
-
-        // The state of each neighbor
-        return view[site];
-      });
-
-      return stat(hood, code, v);
-    };
-
-    // Clipboard, zero filled
-    var grid = new Uint8Array(size);
-    var next = seed;
-
-    // Tick
-    return function () {
-      grid = grid.map(next);
-      next = step;
-
-      return grid;
-    };
+    return grid;
   };
+};
 
-  return Otto;
+return Otto;
 
 }());
-//# sourceMappingURL=otto.js.map
--- index.es
+++ index.es
@@ -4,13 +4,14 @@
 // Wrap index round edges
 // http://stackoverflow.com/questions/1082917/mod-of-negative-number-is-melting-my-brain
 const myMod = (a, b) => a - (b * Math.floor(a / b));
-const zeros = (1024).toString(2).split('').slice(1).join('');
-const zerosMax = zeros.length;
 
 // Rule to binary convert
 const parseRule = (rule) => {
   // Base 2 digits
   const code = Number(rule).toString(2);
+
+  const zeros = (1024).toString(2).split('').slice(1).join('');
+  const zerosMax = zeros.length;
 
   // No padding past 10
   const diff = Math.max(zerosMax, zerosMax - code.length);
@@ -19,39 +20,36 @@ const parseRule = (rule) => {
   return `${zeros}${code}`.substr(diff).split('').reverse();
 };
 
-// Defaults
-const data = {
-  size: 1,
-  rule: 30,
-
-  // How far from center lie the neighbors
-  ends: [-1, 0, 1],
-
-  // Flip middle cell
-  seed: (v, i, view) => i === Math.floor(view.length * 0.5),
-
-  // Index based lookup
-  stat: (hood, code) => {
-    const flags = hood.join('').toString(2);
-    const stats = parseInt(flags, 2);
-
-    return code[stats];
-  },
-};
-
-// Setup
-const Otto = (opts) => {
+// Maker
+const Otto = (options) => {
   // Merge options and defaults
-  const { size, rule, ends, stat, seed } = Object.assign({}, data, opts);
+  const settings = Object.assign({
+    size: 1,
+    rule: 30,
+
+    // How far from center lie the neighbors
+    ends: [-1, 0, 1],
+
+    // Flip middle cell
+    seed: (v, i, view) => i === Math.floor(view.length * 0.5),
+
+    // Index based lookup
+    stat: (hood, code) => {
+      const flags = hood.join('').toString(2);
+      const stats = parseInt(flags, 2);
+
+      return code[stats];
+    },
+  }, options);
 
   // Rule 90 would be
   // ```['0', '1', '0', '1', '1', '0', '1']```
-  const code = parseRule(rule);
+  const code = parseRule(settings.rule);
 
   // Calculate state
   const step = (v, i, view) => {
     // Collect neighboring flags
-    const hood = ends.map((span) => {
+    const hood = settings.ends.map((span) => {
       // The index for each neighbor
       const site = myMod(span + i, view.length);
 
@@ -59,12 +57,12 @@ const Otto = (opts) => {
       return view[site];
     });
 
-    return stat(hood, code, v);
+    return settings.stat(hood, code, v);
   };
 
   // Clipboard, zero filled
-  let grid = new Uint8Array(size);
-  let next = seed;
+  let grid = new Uint8Array(settings.size);
+  let next = settings.seed;
 
   // Tick
   return () => {
