50943b7a27e8caed11521deff6ebdd0595556df7
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-07-04 21:25:51 +0200

    module: rebuild cjs version using buble

--- index.js
+++ index.js
@@ -5,27 +5,25 @@
 
 // Wrap index round edges
 // http://stackoverflow.com/questions/1082917/mod-of-negative-number-is-melting-my-brain
-var myMod = function myMod(a, b) {
-  return a - b * Math.floor(a / b);
-};
+var myMod = function (a, b) { return a - (b * Math.floor(a / b)); };
 
 // Rule to binary convert
-var parseRule = function parseRule(rule) {
+var parseRule = function (rule) {
   // Base 2 digits
   var code = Number(rule).toString(2);
 
-  var zeros = 1024 .toString(2).split('').slice(1).join('');
+  var zeros = (1024).toString(2).split('').slice(1).join('');
   var zerosMax = zeros.length;
 
   // No padding past 10
   var diff = Math.max(zerosMax, zerosMax - code.length);
 
   // Zero pad ruleset if need be
-  return ('' + zeros + code).substr(diff).split('').reverse();
+  return ("" + zeros + code).substr(diff).split('').reverse();
 };
 
 // Maker
-var Otto = function Otto(data) {
+var Otto = function (data) {
   // Merge options and defaults
   var t0to = Object.assign({
     size: 1,
@@ -35,17 +33,15 @@ var Otto = function Otto(data) {
     ends: [-1, 0, 1],
 
     // Flip middle cell
-    seed: function seed(v, i, view) {
-      return i === Math.floor(view.length * 0.5);
-    },
+    seed: function (v, i, view) { return i === Math.floor(view.length * 0.5); },
 
     // Index based lookup
-    stat: function stat(hood, code) {
+    stat: function (hood, code) {
       var flags = hood.join('').toString(2);
       var stats = parseInt(flags, 2);
 
       return code[stats];
-    }
+    },
   }, data);
 
   // Rule 90 would be
@@ -53,7 +49,7 @@ var Otto = function Otto(data) {
   var code = parseRule(t0to.rule);
 
   // Calculate state
-  var step = function step(v, i, view) {
+  var step = function (v, i, view) {
     // Collect neighboring flags
     var hood = t0to.ends.map(function (span) {
       // The index for each neighbor
@@ -80,3 +76,4 @@ var Otto = function Otto(data) {
 };
 
 module.exports = Otto;
+
